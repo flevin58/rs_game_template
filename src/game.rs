dae94b57b81cb41dc5c97487a22225d511961b04
Fernando Julio Levin <flevin58@gmail.com>
2024-11-01 16:48:45 +0100

    Changed  exit key to ESC

--- src/game.rs
+++ src/game.rs
@@ -1,5 +1,5 @@
-use raylib::prelude::*;
 use raylib::consts::KeyboardKey::*;
+use raylib::prelude::*;
 
 const WINDOW_WIDTH: i32 = 640;
 const WINDOW_HEIGHT: i32 = 480;
@@ -16,27 +16,28 @@ impl Game {
 
         r.set_target_fps(FPS);
         r.set_window_size(WINDOW_WIDTH, WINDOW_HEIGHT);
-        r.set_exit_key(Some(KEY_F12));
+        r.set_exit_key(Some(KEY_ESCAPE));
 
-
-        Game{
-            rl: r,
-            thread: t,
-        }
+        Game { rl: r, thread: t }
     }
 
-    pub fn update(&self) {
-    }
+    pub fn update(&self) {}
 
     pub fn draw(&mut self) {
         let mut d = self.rl.begin_drawing(&self.thread);
         d.clear_background(Color::BLACK);
 
         d.draw_circle(100, 100, 50.0, Color::GREEN);
-        d.draw_text("Press F12 to exit", 20, WINDOW_HEIGHT-30, 24, Color::WHITE);
+        d.draw_text(
+            "Press ESC to exit",
+            20,
+            WINDOW_HEIGHT - 30,
+            24,
+            Color::WHITE,
+        );
     }
 
-    pub fn has_finished(&self) -> bool{
+    pub fn has_finished(&self) -> bool {
         self.rl.window_should_close()
     }
 }
